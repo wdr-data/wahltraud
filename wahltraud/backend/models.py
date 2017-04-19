caf780ab9fc0faa63dea6ceede86f3acb8a613cd
Jannes Höke <mail@jannes-hoeke.de>
2017-04-19 23:10:16 +0200

    add short_title to Entry model

--- wahltraud/backend/models.py
+++ wahltraud/backend/models.py
@@ -9,6 +9,11 @@ class Entry(models.Model):
         verbose_name_plural = 'Einträge'
 
     title = models.CharField('Titel', max_length=200, null=False)
+    short_title = models.CharField(
+        'Kurzer Titel',
+        help_text='Wird auf den Link-Buttons angezeigt (max. 20 Zeichen)',
+        max_length=20,
+        null=False)
     text = models.CharField('Text', max_length=640, null=False)
     link_one = models.ForeignKey(
         'self', verbose_name='Link 1', related_name='+', null=True, blank=True)
@@ -20,4 +25,4 @@ class Entry(models.Model):
     pub_date = models.DateTimeField('Veröffentlicht am', default=timezone.now)
 
     def __str__(self):
-        return '%s (%s)' % (self.title, self.pub_date.strftime('%d.%m.%Y'))
+        return '%s (%s)' % (self.short_title, self.pub_date.strftime('%d.%m.%Y'))
