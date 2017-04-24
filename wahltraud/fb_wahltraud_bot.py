cd125854cbd9eb740c76d70a9db2466fcdfbe977
LizzAy <lisachenbach@gmx.de>
2017-04-24 11:00:52 +0200

    changed new model names giving back random info

--- wahltraud/fb_wahltraud_bot.py
+++ wahltraud/fb_wahltraud_bot.py
@@ -5,10 +5,10 @@ import csv
 
 from flask import Flask, request
 import requests
-#from django.utils.timezone import localtime, now
+import random
 from django.utils import timezone
 
-from backend.models import Entry
+from backend.models import Entry, FacebookUser
 
 # TODO: The idea is simple. When you send "subscribe" to the bot, the bot server would add a record according to the sender_id to their
 # database or memory , then the bot server could set a timer to distribute the news messages to those sender_id who have subscribed for the news.
@@ -54,8 +54,8 @@ def handle_messages(data):
             if text == '/wahltraud':
                 reply = "Hier kannst du richtig was lernen..."
                 send_text(sender_id, reply)
-                #data = get_data()
-                #send_list_template(data, sender_id)
+                random_info = get_data()
+                send_text_with_button(sender_id, random_info.title)
             elif text == '/config':
                 reply = "Hier kannst du deine facebook Messenger-ID hinterlegen um automatisch " \
                         "Infos zu den wichtigsten Begriffen rund um die Wahl von uns zu erhalten.\n" \
@@ -64,44 +64,47 @@ def handle_messages(data):
             else:
                 reply = "echo: " + text
                 send_text(sender_id, reply)
-        elif "postback" in event and event['postback'].get("payload", "") == "start":
-            reply = "Herzlich willkommen im WahltraudMessenger. \n\n" \
-                    "Dies ist der Bot zur NRW Landtagswahl 2017. " \
-                    "Ich liefere dir jeden Tag Infos zu den wichtigsten Begriffen rund um die Wahl." \
-                    "\n\nWas Du dafür tun musst: Fast nichts. Tippe \"/wahltraud\" für dein Update."
-            send_text(sender_id, reply)
-        elif "postback" in event and event['postback'].get("payload", "").split("#")[0] == "info":
-            requested_info_id = event['postback'].get("payload", "").split("#")[1]
-            info = Info.objects.get(id=int(requested_info_id))
-            status = "intro"
-            send_text(sender_id, info.headline)
-            if info.media != "":
-                image = "https://infos.data.wdr.de/backend/static/media/" + str(info.media)
-                send_image(sender_id, image)
-            logger.debug("status in postback: " + status)
-            send_text_with_button(sender_id, info, status)
-        elif "postback" in event and event['postback'].get("payload", "").split("#")[0] == "more":
-            requested_info_id = event['postback'].get("payload", "").split("#")[2]
-            info = Info.objects.get(id=int(requested_info_id))
-            logger.debug(event['postback'].get("payload", "").split("#")[1])
-            if event['postback'].get("payload", "").split("#")[1] == "0":
-                status = "one"
-            elif event['postback'].get("payload", "").split("#")[1] == "1":
-                status = "two"
-            send_text_with_button(sender_id, info, status)
-            # reply = "Hier folgen weitere Infos zum Thema."
-            # send_text(sender_id, reply)
+        # elif "postback" in event and event['postback'].get("payload", "") == "start":
+        #     reply = "Herzlich willkommen im WahltraudMessenger. \n\n" \
+        #             "Dies ist der Bot zur NRW Landtagswahl 2017. " \
+        #             "Ich liefere dir jeden Tag Infos zu den wichtigsten Begriffen rund um die Wahl." \
+        #             "\n\nWas Du dafür tun musst: Fast nichts. Tippe \"/wahltraud\" für dein Update."
+        #     send_text(sender_id, reply)
+        # elif "postback" in event and event['postback'].get("payload", "").split("#")[0] == "info":
+        #     requested_info_id = event['postback'].get("payload", "").split("#")[1]
+        #     info = Info.objects.get(id=int(requested_info_id))
+        #     status = "intro"
+        #     send_text(sender_id, info.headline)
+        #     if info.media != "":
+        #         image = "https://infos.data.wdr.de/backend/static/media/" + str(info.media)
+        #         send_image(sender_id, image)
+        #     logger.debug("status in postback: " + status)
+        #     send_text_with_button(sender_id, info, status)
+        # elif "postback" in event and event['postback'].get("payload", "").split("#")[0] == "more":
+        #     requested_info_id = event['postback'].get("payload", "").split("#")[2]
+        #     info = Info.objects.get(id=int(requested_info_id))
+        #     logger.debug(event['postback'].get("payload", "").split("#")[1])
+        #     if event['postback'].get("payload", "").split("#")[1] == "0":
+        #         status = "one"
+        #     elif event['postback'].get("payload", "").split("#")[1] == "1":
+        #         status = "two"
+        #     send_text_with_button(sender_id, info, status)
         elif "postback" in event and event['postback'].get("payload", "").split("#")[0] == "subscribe":
             subscribe_user(sender_id)
             reply = "Danke für deine Anmeldung!\nDu erhältst nun ein tägliches Update jeweils um 8:00 Uhr wochentags."
             send_text(sender_id, reply)
         elif "postback" in event and event['postback'].get("payload", "") == "back":
-            data = get_data()
-            send_list_template(data, sender_id)
+            reply = "Zurück..."
+            send_text(sender_id, reply)
+            # data = get_data()
+            # send_list_template(data, sender_id)
 
 def get_data():
     today = timezone.localtime(timezone.now()).date()
-    return Info.objects.filter(pub_date__date=today)[:4]
+    info_list = list(Entry.objects.all())
+    random_info = random.choice(info_list)
+    logger.debug('Random Info Title: ' + random_info.title)
+    return random_info #Info.objects.filter(pub_date__date=today)[:4]
 
 def subscribe_user(user_id):
     FacebookUser.objects.create(uid = user_id)
@@ -249,9 +252,6 @@ def send_text_with_button(recipient_id, info, status="other"):
         status_id = 3
         text = info
         button_title = "OK"
-
-    logger.debug("Text: " + str(text) + ", info.introtext: " + str(info.intro_text))
-    logger.debug("Status at send_text_with_button: " + str(status) + " " + str(status_id))
 
     if status_id == 3:
         task = 'subscribe#' + recipient_id
