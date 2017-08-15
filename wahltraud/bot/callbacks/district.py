90fa03b304d65e0fe8a0e95acd7730235b88970f
Jannes Höke <jhoeke@uni-bremen.de>
2017-08-15 09:39:07 +0200

    district WIP

--- wahltraud/bot/callbacks/district.py
+++ wahltraud/bot/callbacks/district.py
@@ -1,6 +1,6 @@
 
 from ..fb import send_buttons, button_postback, send_text
-from ..data import by_uuid
+from ..data import by_uuid, by_plz
 
 
 def find_district(event, parameters, **kwargs):
@@ -14,8 +14,23 @@ deine Postleitzahl schreibst."""
 
         send_text(sender_id, reply)
 
-    elif plz:
-        send_district(sender_id, plz)  # TODO
+    else:
+        district_uuids = by_plz.get(plz)
+
+        if not district_uuids:
+            send_text(sender_id, "Diese PLZ sagt mir nichts...")
+
+        elif len(district_uuids) == 1:
+            send_district(sender_id, by_plz[plz])
+
+        else:
+            send_buttons(sender_id,
+                         'Welchen Wahlkreis meinst du?',
+                         [button_postback(district['district'],
+                                          {'show_district': district['district_uuid']})
+                          for district in
+                          [by_uuid[uuid] for uuid in district_uuids]]
+                         )
 
 
 def show_district(event, payload, **kwargs):
@@ -28,5 +43,5 @@ def send_district(sender_id, district_uuid):
     send_buttons(sender_id,
                  'Ok. Der Wahlkreis deiner Wahl ist {district}'.format(
                      district=by_uuid[district_uuid]['name']),
-                 button_postback("Zeige Wahlkreis-Info",
-                                 {'show_district': district_uuid}))
+                 [button_postback("Zeige Wahlkreis-Info",
+                                  {'show_district': district_uuid})])
--- wahltraud/bot/data/__init__.py
+++ wahltraud/bot/data/__init__.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 DATA_DIR = Path(__file__).absolute().parent
 
 candidate_list = json.load(open(str(DATA_DIR/'alle_kandidaten.json')))['list']
-district_list = json.load(open(str(DATA_DIR/'wahlkreis_info.json')))['wahlkreise']
+district_list = json.load(open(str(DATA_DIR/'wahlkreis_info.json')))['districts']
 
 by_first_name = defaultdict(set)
 by_last_name = defaultdict(set)
@@ -14,8 +14,8 @@ by_plz = defaultdict(set)
 by_uuid = dict()
 
 for candidate in candidate_list:
-    by_first_name[candidate['vorname']].add(candidate['uuid'])
-    by_last_name[candidate['nachname']].add(candidate['uuid'])
+    by_first_name[candidate['first_name']].add(candidate['uuid'])
+    by_last_name[candidate['last_name']].add(candidate['uuid'])
     by_uuid[candidate['uuid']] = candidate
 
 for district in district_list:
