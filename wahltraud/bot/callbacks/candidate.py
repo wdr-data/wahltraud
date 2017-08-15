145509ac42f81d9c0de5f6d527935b6785b39f98
LizzAy <lisachenbach@gmx.de>
2017-08-15 10:23:44 +0200

    show candidate info

--- wahltraud/bot/callbacks/candidate.py
+++ wahltraud/bot/callbacks/candidate.py
@@ -13,10 +13,24 @@ def basics(event, parameters, **kwargs):
     candidate = dict()
     candidate = find_candidates(first_name, last_name)
     logger.debug('candidate information: ' + str(candidate)) #['first_name'] + ' ' + candidate['last_name'] + ' Partei: ' + candidate['party'] + ' Jahrgang: ' + candidate['age'])
-    send_text(
-        sender_id,
-        'Du möchtest etwas über {first_name} {last_name} erfahren?'.format(
-            first_name=first_name, last_name=last_name))
+    send_buttons(sender_id, """
+    {first_name}{last_name}
+    Partei: {party}
+    Alter/ Jahrgang: {age}
+    """.format(
+        first_name=candidate['first_name'],
+        last_name=candidate['last_name'],
+        party=candidate['party'],
+        age=candidate['age']
+    ),
+                 [
+                     button_postback("Mehr Info", {'more_infos': candidate['uuid']}),
+                     button_postback("Anderer Kandidat", {'candidate_check': candidate['uuid']})
+                 ])
+    #send_text(
+    #    sender_id,
+    #    'Du möchtest etwas über {first_name} {last_name} erfahren?'.format(
+    #        first_name=first_name, last_name=last_name))
 
 
 def candidate_check(event, **kwargs):
