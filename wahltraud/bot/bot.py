69f384239eb15f8231513f9ceeb2f7ba25ad3700
ChristianJoe <cjoe@gmx.de>
2017-08-27 22:29:12 +0200

    choose electorial programm

--- wahltraud/bot/bot.py
+++ wahltraud/bot/bot.py
@@ -53,7 +53,7 @@ def make_event_handler():
         ApiAiHandler(party.basics, 'parteien'),
         PayloadHandler(party.show_parties, ['show_parties']),
         PayloadHandler(party.show_electorial, ['show_electorial']),
-        PayloadHandler(party.show_electorial, ['show_party_options']),
+        PayloadHandler(party.show_party_options, ['show_party_options']),
         ApiAiHandler(candidate.candidate_check, 'kandidatencheck'),
         PayloadHandler(district.intro_district, ['intro_district']),
         PayloadHandler(candidate.intro_candidate, ['intro_candidate']),
