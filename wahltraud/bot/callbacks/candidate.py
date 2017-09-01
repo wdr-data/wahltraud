4116660bcbb4efea9c1e027bdf7eab62bfe937ff
ChristianJoe <cjoe@gmx.de>
2017-09-01 10:21:05 +0200

    nrw candidate without district

--- wahltraud/bot/callbacks/candidate.py
+++ wahltraud/bot/callbacks/candidate.py
@@ -189,7 +189,6 @@ def more_infos_nrw(event, payload, **kwargs):
     candidate = by_uuid[candidate_uuid]
 
 
-    candidate_district_id = district['district_id']
 
     logger.info('Kandidatencheck - mehr Infos zu {name} von Partei {party}'.format(
         name=' '.join(filter(bool, (candidate['degree'],
@@ -209,6 +208,7 @@ def more_infos_nrw(event, payload, **kwargs):
     if 'district_uuid':
         district_uuid = candidate['district_uuid']
         district = by_uuid[district_uuid]
+        candidate_district_id = district['district_id']
         buttons.insert(0,button_postback("Info Wahlkreis " + candidate_district_id, {'show_district': district_uuid}))
 
     if candidate['nrw']['video'] is not None:
