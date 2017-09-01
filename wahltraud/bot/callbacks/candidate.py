0c754d3f10bbf875e5923256c660f2805d4fd562
ChristianJoe <cjoe@gmx.de>
2017-09-01 11:31:20 +0200

    quick reply after video

--- wahltraud/bot/callbacks/candidate.py
+++ wahltraud/bot/callbacks/candidate.py
@@ -279,7 +279,10 @@ def show_video(event, payload, **kwargs):
 
     send_text(
         sender_id,
-        "",
+        "WDR-Kandidatencheck Interview von {first_name}{last_name} (4 Minuten).".format(
+            first_name = candidate['first_name'],
+            last_name = candidate['last_name']
+        ),
         quick_replies)
 
 
