5c7986bf79e6aadb077a3e1883896149c883efa6
LizzAy <lisachenbach@gmx.de>
2017-09-01 17:14:29 +0200

    turnedn first sentence off for todays push

--- wahltraud/bot/callbacks/shared.py
+++ wahltraud/bot/callbacks/shared.py
@@ -50,8 +50,8 @@ def get_breaking():
 
 
 def schema(data, user_id):
-    reply = "Hier kommt dein tägliches Update zur Bundestagswahl"
-    send_text(user_id, reply)
+    # reply = "Hier kommt dein tägliches Update zur Bundestagswahl"
+    # send_text(user_id, reply)
     reply = ""
     first_id = None
 
