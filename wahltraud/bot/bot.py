1d7b353257d747888ce062b29e4e271fab78848c
ChristianJoe <cjoe@gmx.de>
2017-09-08 10:49:01 +0200

    presidents

--- wahltraud/bot/bot.py
+++ wahltraud/bot/bot.py
@@ -60,7 +60,7 @@ def make_event_handler():
         PayloadHandler(more_data, ['more_data']),
         PayloadHandler(menue_manifesto, ['menue_manifesto']),
         PayloadHandler(about_manifesto, ['about_manifesto']),
-        ApiAIHandler(presidents, 'bundespräsident'),
+        ApiAiHandler(presidents, 'bundespräsident'),
         ApiAiHandler(candidate.basics, 'kandidat'),
         ApiAiHandler(party.basics, 'parteien'),
         ApiAiHandler(party.top_candidates_apiai, 'spitzenkandidat'),
