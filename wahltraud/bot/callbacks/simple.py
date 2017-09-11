f8e0402198fd81d607992a508aacb2e0ec0b5b06
LizzAy <lisachenbach@gmx.de>
2017-09-11 14:36:06 +0200

    get uuid to return WK Info

--- wahltraud/bot/callbacks/simple.py
+++ wahltraud/bot/callbacks/simple.py
@@ -46,7 +46,7 @@ def get_started(event, **kwargs):
     if referral:
         ref = referral.get('ref')
         wk = int(ref.replace("WK", ""))
-        district_uuid = by_district_id(str(wk))
+        district_uuid = by_district_id.get(wk)
         logging.info('Bot wurde mit neuem User geteilt: ' + ref + ' WK uuid: ' + district_uuid)
 
         reply = """
