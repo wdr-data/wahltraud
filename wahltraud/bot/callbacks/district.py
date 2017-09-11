de757e943a069268a6bba339ebc64dfa9e516124
ChristianJoe <cjoe@gmx.de>
2017-09-11 18:35:32 +0200

    buttons

--- wahltraud/bot/callbacks/district.py
+++ wahltraud/bot/callbacks/district.py
@@ -104,7 +104,7 @@ Das Durchschnittsalter der Kandidaten beträgt {avg_age} Jahre.
                      button_postback("Kandidaten", {'show_candidates': district_uuid}),
                      #button_postback("Bundestagswahl 2013", {'show_13': district_uuid}),
                      button_postback("Info Wahlkreis "+ district['district_id'], {'show_structural_data': district_uuid}),
-                     button_postback("Bundestagswahl 2013", {'show_13': district_uuid})
+                     button_postback("Ergebnis Wahl '13", {'show_13': district_uuid})
                      #button_postback("Anderer Wahlkreis", ['intro_district']),
                  ])
 
@@ -251,6 +251,6 @@ Bevölkerung pro km²: {perm2}
     ),
                  [
                      button_postback("Kandidaten", {'show_candidates': district_uuid}),
-                    button_postback("Ergebnisse Wahl '13", {'show_13': district_uuid}),
+                    button_postback("Ergebnis Wahl '13", {'show_13': district_uuid}),
                      button_postback("Anderer Wahlkreis", ['intro_district']),
                  ])
