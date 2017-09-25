77ef12cdef5d3ec0cbc7ad22151b2771b6cc9edd
LizzAy <lisachenbach@gmx.de>
2017-09-25 08:21:50 +0200

    no novi button, added result_17 instead

--- wahltraud/bot/callbacks/district.py
+++ wahltraud/bot/callbacks/district.py
@@ -114,9 +114,9 @@ Das Durchschnittsalter der Kandidaten beträgt {avg_age} Jahre.
                  [
                      button_postback("Kandidaten", {'show_candidates': district_uuid}),
                      button_postback("Wahlkreis in Zahlen", {'show_structural_data': district_uuid}),
-                     button_postback("Ergebnis per Push", {'novi': district_uuid}),
+                    #  button_postback("Ergebnis per Push", {'novi': district_uuid}),
                     #  button_postback("Ergebnis Wahl '13", {'show_13': district_uuid})
-                    #  button_postback("Ergebnis 2017", {'result_17': district_uuid})
+                     button_postback("Ergebnis 2017", {'result_17': district_uuid}),
                      #button_postback("Anderer Wahlkreis", ['intro_district']),
                  ])
 
